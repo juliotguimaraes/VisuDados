9beb50e07c943c0a4e851e03e1c09dbfcf6205a5
Julio Guimaraes <julioctg33@gmail.com>
2016-06-27 20:15:29 -0300

    script para grafico de linha. Com problema de tamanho

--- TP4/script.js
+++ TP4/script.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$( document ).ready(function(){
 
   $('.number1').each(function () {
     $(this).prop('Counter',0).animate({
@@ -318,5 +318,74 @@ for (g in groups3){
 
   d3.select(self.frameElement).style("height", diameter + "px");
 
+//Line chart
 
+var margin = {top: 20, right: 20, bottom: 50, left: 50},
+    width = 500 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
+
+// Parse the date / time
+var parseDate = d3.time.format("%Y").parse;
+
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+    .orient("bottom").ticks(5);
+
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(5);
+
+// Define the line
+var valueline = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.wage); })
+    .interpolate("linear");
+    
+// Adds the svg canvas
+var svg = d3.select("#chart3")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("line.csv", function(error, data) {
+    data.forEach(function(d) {
+        d.date = parseDate(d.date);
+        d.wage = +d.wage;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.wage; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+        
+    svg.append('svg:path')
+				.attr('d', valueline(data2))
+				.attr('stroke', 'blue')
+				.attr('stroke-width', 2)
+				.attr('fill', 'none');
+
+});
+	
 });
